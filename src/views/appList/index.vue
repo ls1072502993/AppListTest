ccb0c5f2afeb1b1674acecb9530817c28bbeccfb
LiSheng <1072502993@qq.com>
2024-08-27 18:49:05 +0800

    no message

--- src/views/appList/index.vue
+++ src/views/appList/index.vue
@@ -25,7 +25,7 @@
         <div class="name text-ellipsis">{{ v['im:name'].label }}</div>
         <div class="type">{{ v.category.attributes.label }}</div>
         <div class="star">
-          <Rate v-model="value" :size="15" color="#ffd21e" void-icon="star" void-color="#eee" />
+          <Rate v-model="starValue" :size="15" color="#ffd21e" void-icon="star" void-color="#eee" />
           <span>(1111)</span>
         </div>
       </div>
@@ -43,50 +43,30 @@ import { _debounce } from '@/utils/utils'
 // 顶部搜索结果
 const keyWord = ref('')
 const getKeyWordList = _debounce(() => {
-  console.log(keyWord.value)
+  hotList.value = allHotList.value.filter(v => v['im:name'].label.toLowerCase().indexOf(keyWord.value.toLowerCase()) >= 0)
 }, 500)
 
 // 获取推荐App列表
-const recommendList = reactive({})
-const getRecommendList = () => {
-  fetch.getTopgrossingapplicationsList(10).then(res => {
-    Object.assign(recommendList, res.data.feed.entry)
-  })
-}
+const recommendList = ref([])
 onMounted(() => {
-  getRecommendList()
+  fetch.getTopgrossingapplicationsList(10).then(res => {
+    recommendList.value = res.data.feed.entry
+  })
 })
 
 // 获取热度最高免费App列表
-const hotList = reactive({})
-const value = ref(3)
-const isLoading = ref(false)
-const pageSize = ref(10)
-const getHotList = () => {
-  isLoading.value = true
-  fetch.getTopfreeapplicationsList(pageSize.value).then(res => {
-    isLoading.value = false
-    Object.assign(hotList, res.data.feed.entry)
-  })
-}
-const loadList = () => {
-  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  let windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
-  let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-  if ((scrollTop + windowHeight) == scrollHeight) {
-    if (isLoading.value || pageSize.value >= 100) return
-    pageSize.value += 10
-    console.log(pageSize.value)
-  }
-}
-onMounted(() => {
-  getHotList()
-  window.addEventListener('scroll', loadList)
-})
+const hotList = ref([])
+const allHotList = ref([])
+const loadingAll = ref(false)
+const starValue = ref(3)
 
-onUnmounted(() => {
-  window.removeEventListener('scroll', loadList)
-}) 
+onMounted(() => {
+  fetch.getTopfreeapplicationsList(100).then(res => {
+    loadingAll.value = true
+    hotList.value = res.data.feed.entry
+    allHotList.value = res.data.feed.entry
+  })
+})
 </script>
 
 <style lang="scss" scoped>
